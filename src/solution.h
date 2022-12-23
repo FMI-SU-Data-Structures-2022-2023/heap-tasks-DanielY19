95dfe32d496ae5602d3d7b78e6e282448619a0e8
DanielY19 <yaganan38@gmail.com>
2022-12-23 18:07:08 +0200

    Solved all tasks.

--- src/solution.h
+++ src/solution.h
@@ -1,10 +1,36 @@
 #ifndef WEEK7_TREE_H
 
 class Trie {
+private:
+    struct Node {
+        const static size_t MAX_ALPHABET = 26;
+
+        bool isWord;
+        Node *next[26];
+
+        Node() : isWord(false) {
+            this->null();
+        }
+
+        static void insert(const char *, Node *);
+
+        static bool search(const char *, Node *);
+
+        static void destroy(Node *);
+
+        void null();
+    };
+
+    Node *start;
 public:
-    void insert(const char*);
-    bool search(const char*);
-    Trie& operator=(const Trie& other) = delete;
+    Trie() : start(new Node) {}
+
+    void insert(const char *);
+
+    bool search(const char *);
+
+    Trie &operator=(const Trie &other) = delete;
+
     ~Trie();
 };
 
--- test/solution.cpp
+++ test/solution.cpp
@@ -1,13 +1,14 @@
 #include "catch2/catch_all.hpp"
 #include "solution.h"
 #include "Heap.hpp"
+
 using namespace std;
 
 TEST_CASE("Validate Heap") {
 
     Heap<int> heap;
     CHECK(heap.isEmpty() == true);
-    for (int i = 100;i >= 10; i--) {
+    for (int i = 100; i >= 10; i--) {
         heap.insert(i);
         CHECK(heap.isEmpty() == false);
     }
@@ -15,28 +16,25 @@ TEST_CASE("Validate Heap") {
     for (int i = 10; i <= 100; i++) {
         heap.removeTop();
         if (i != 100) {
-            CHECK(heap.getTop() == (i+1));
+            CHECK(heap.getTop() == (i + 1));
         }
     }
     CHECK(heap.isEmpty() == true);
 }
 
 
-
 TEST_CASE("Validate Trie") {
+    Trie trie;
+    trie.insert("hello");
+    trie.insert("world");
+    trie.insert("one");
+    trie.insert("two");
+    trie.insert("asdasdasdasd");
+    trie.insert("asdasdasdasdasd");
 
-   Trie trie;
-   trie.insert("hello");
-   trie.insert("world");
-   trie.insert("one");
-   trie.insert("two");
-   trie.insert("asdasdasdasd");
-   trie.insert("asdasdasdasdasd");
-   CHECK(trie.search("hello") == true);
-   CHECK(trie.search("asdasdasdasd") == true);
-
-   CHECK(trie.search("nothing") == false);
-   CHECK(trie.search("worl") == false);
-   CHECK(trie.search("asdasdasdasdas") == true);
-
+    CHECK(trie.search("hello"));
+    CHECK(trie.search("asdasdasdasd"));
+    CHECK_FALSE(trie.search("nothing"));
+    CHECK_FALSE(trie.search("worl"));
+    CHECK_FALSE(trie.search("asdasdasdasdas"));
 }
